37d91db2bdf8cb55611e1e9d2ec5951987c40b06
Sean Marlow <sean.marlow@suse.com>
2025-01-24 08:57:42 -0500

    Revert to attributes that start with lowercase

--- aws/sqs_event_manager/sqs_event_manager/app.py
+++ aws/sqs_event_manager/sqs_event_manager/app.py
@@ -106,10 +106,10 @@ def process_message(record: dict, batch_item_failures: dict):
                 return error_response(400, entitlements.error)
 
             request = {  # noqa TODO: Cleanup noqa
-                'CustomerIdentifier': message.customer_id,
-                'MarketplaceIdentifier': 'AWS',
-                'ProductCode': message.product_code,
-                'Entitlements': entitlements.get_entitlements()
+                'customerIdentifier': message.customer_id,
+                'marketplaceIdentifier': 'AWS',
+                'productCode': message.product_code,
+                'entitlements': entitlements.get_entitlements()
             }
             # TODO: Send entitlement update request with entitlement info
         else:
